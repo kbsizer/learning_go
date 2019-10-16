0cf6893f1f4ec9917d897a2cd0f88936367f6cce
Kenneth Sizer <Kenneth.Sizer@sas.com>
2019-10-16 09:56:10 -0400

    Adding Gophercises

---
+++ gophercises/1_QuizGame/main.go
@@ -0,0 +1,20 @@
+// Gophercise 1 - Quiz Game - https://gophercises.com/exercises/quiz
+//
+// Packages used
+//		log  -- logging -- https://www.datadoghq.com/blog/go-logging/
+//		flag -- command-line args -- https://blog.alexellis.io/5-keys-to-a-killer-go-cli/
+package main
+
+import "log"
+
+type questionAnswer struct {
+	question string
+	answer   string
+}
+
+func main() {
+	log.Print("Gophercise 1 - Quiz Game - https://gophercises.com/exercises/quiz")
+	// process command line args
+
+	// load CSV file
+}
